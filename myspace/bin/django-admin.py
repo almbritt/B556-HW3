ccb150a777eb1421df35269a3c815cf9daca38a6
almbritt <55154198+almbritt@users.noreply.github.com>
2020-03-04 18:27:30 -0500

    Simple Django Page

---
+++ myspace/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/alexisbritt/Desktop/Bio Database Managment B556/Django Example/myspace/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
